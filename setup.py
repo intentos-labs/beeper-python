d8770aa605948d31a9540c6190eddd72ab210d28
mossv2 <mossv2@github.com>
2025-04-10 15:29:43 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="beeper",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+        "beeper": ["solc/**/*"],
+    },
     install_requires=[
         "web3>=7.10.0",
         "python-dotenv>=1.1.0",
